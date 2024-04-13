e806351463a0c14dcc6865eca6a8170b96d0aec5
Sunil K <23735109+SunilCode@users.noreply.github.com>
2024-04-13 17:18:17 +0530

    Update MarketData.py

--- MarketData.py
+++ MarketData.py
@@ -1,1 +1,15 @@
-{"metadata":{"kernelspec":{"language":"python","display_name":"Python 3","name":"python3"},"language_info":{"name":"python","version":"3.10.13","mimetype":"text/x-python","codemirror_mode":{"name":"ipython","version":3},"pygments_lexer":"ipython3","nbconvert_exporter":"python","file_extension":".py"},"kaggle":{"accelerator":"none","dataSources":[{"sourceId":8088112,"sourceType":"datasetVersion","datasetId":4774652},{"sourceId":8105785,"sourceType":"datasetVersion","datasetId":4787440}],"dockerImageVersionId":30684,"isInternetEnabled":true,"language":"python","sourceType":"notebook","isGpuEnabled":false}},"nbformat_minor":4,"nbformat":4,"cells":[{"cell_type":"code","source":"# This Python 3 environment comes with many helpful analytics libraries installed\n# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python\n# For example, here's several helpful packages to load\n\nimport numpy as np # linear algebra\nimport pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)\n\n# Input data files are available in the read-only \"../input/\" directory\n# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory\n\nimport os\nfor dirname, _, filenames in os.walk('/kaggle/input'):\n    for filename in filenames:\n        print(os.path.join(dirname, filename))\n\n# You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using \"Save & Run All\" \n# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session","metadata":{"_uuid":"8f2839f25d086af736a60e9eeb907d3b93b6e0e5","_cell_guid":"b1076dfc-b9ad-4769-8c92-a6c4dae69d19","trusted":true},"execution_count":null,"outputs":[]},{"cell_type":"code","source":"pip install yfinance --upgrade --no-cache-dir","metadata":{"trusted":true},"execution_count":null,"outputs":[]},{"cell_type":"code","source":"import yfinance as yf\nimport pandas as pd\n\nnse_tickers_df = pd.read_csv('/kaggle/input/nsetickerlist/NSE_EQUITY_List.csv')\nnse_tickers_df['SYMBOL'] = nse_tickers_df['SYMBOL'].astype(str) + '.NS'\n\ndf = pd.DataFrame()  # Create an empty DataFrame\n\nfor ticker in nse_tickers_df.SYMBOL:\n    tckr_info = yf.Ticker(ticker).info\n    tckr_df = pd.DataFrame([tckr_info])  # Create a DataFrame\n    df = pd.concat([df, tckr_df], axis=0, ignore_index=True, sort=False)  # Concatenate the DataFrames\n\ndf.to_csv('All_ticker_data.csv', index=False)","metadata":{"trusted":true},"execution_count":null,"outputs":[]}]}
+# Download NSE Market data
+import yfinance as yf
+import pandas as pd
+
+nse_tickers_df = pd.read_csv('/kaggle/input/nsetickerlist/NSE_EQUITY_List.csv')
+nse_tickers_df['SYMBOL'] = nse_tickers_df['SYMBOL'].astype(str) + '.NS'
+
+df = pd.DataFrame()  # Create an empty DataFrame
+
+for ticker in nse_tickers_df.SYMBOL:
+    tckr_info = yf.Ticker(ticker).info
+    tckr_df = pd.DataFrame([tckr_info])  # Create a DataFrame
+    df = pd.concat([df, tckr_df], axis=0, ignore_index=True, sort=False)  # Concatenate the DataFrames
+
+df.to_csv('All_ticker_data.csv', index=False)
